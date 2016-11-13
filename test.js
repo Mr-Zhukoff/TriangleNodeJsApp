eed79b98f2f6e2bb36bd661d207bd1108bb324e9
Grigory Zhukov <grigory@zhukoff.pro>
2016-11-13 21:00:59 +0300

    testing

---
+++ test.js
@@ -0,0 +1,12 @@
+﻿var http = require('http');
+
+var server = http.createServer(function (req, res) {
+    res.writeHead(200, {
+        'Content-Type': 'text/html'
+    });
+    res.end('Hello World');
+});
+
+var port = process.env.PORT || 3000;
+
+server.listen(port);
