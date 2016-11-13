92d2a44ed0fe2fba7a651bbc59e53b9bd832fd51
Grigory Zhukov <grigory@zhukoff.pro>
2016-11-13 21:04:09 +0300

    changed to app.js

--- app.js
+++ app.js
@@ -2,7 +2,6 @@
 var exphbs = require('express-handlebars');
 var path = require('path');
 var favicon = require('serve-favicon');
-var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
@@ -18,11 +17,10 @@ app.set('view engine', 'handlebars');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(__dirname + '/public/favicon.ico'));
-app.use(logger('dev'));
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(require('stylus').middleware(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
