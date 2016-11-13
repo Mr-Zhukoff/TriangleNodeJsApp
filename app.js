22ddbc12262d02d436e5c145ab056c314f312617
Grigory Zhukov <grigory@zhukoff.pro>
2016-11-13 21:12:24 +0300

    ccc

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 ﻿var express = require('express');
 var exphbs = require('express-handlebars');
 var path = require('path');
-var favicon = require('serve-favicon');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
+//var favicon = require('serve-favicon');
+//var cookieParser = require('cookie-parser');
+//var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
 //var users = require('./routes/users');
@@ -17,10 +17,10 @@ app.set('view engine', 'handlebars');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(__dirname + '/public/favicon.ico'));
-
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
+//app.use(bodyParser.json());
+//app.use(bodyParser.urlencoded({ extended: false }));
+//app.use(cookieParser());
+ 
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
